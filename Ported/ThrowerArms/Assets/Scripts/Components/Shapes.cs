169f564aab81c1bf58a974d0b9a7e4e011cee241
Kevin Ratelle <kevinratelle@hotmail.com>
2020-02-26 14:52:36 -0500

    Add shapes data and collision (detect and process) systems

---
+++ Ported/ThrowerArms/Assets/Scripts/Components/Shapes.cs
@@ -0,0 +1,18 @@
+﻿
+using Unity.Entities;
+
+public struct Sphere : IComponentData
+{
+    public float Radius;
+}
+
+public struct Cylinder : IComponentData
+{
+    public float Length;
+    public float Radius;
+}
+
+public struct Target : IComponentData
+{
+    public Entity Value;
+}
---
+++ Ported/ThrowerArms/Assets/Scripts/Systems/DetectCollision.cs
@@ -0,0 +1,90 @@
+﻿
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+public struct Sphere : IComponentData
+{
+    public float Radius;
+}
+
+public struct Cylinder : IComponentData
+{
+    public float Length;
+    public float Radius;
+}
+
+public struct Target : IComponentData
+{
+    public Entity Value;
+}
+
+[BurstCompile]
+struct DetectCollisionJob : IJobForEachWithEntity<Sphere, Translation, Target>
+{
+    [WriteOnly] public NativeQueue<ContactPoint>.ParallelWriter Contacts;
+    [ReadOnly] public ComponentDataFromEntity<Cylinder> Cylinders;
+    [ReadOnly] public ComponentDataFromEntity<Translation> Positions;
+    [ReadOnly] public ComponentDataFromEntity<Rotation> Rotations;
+
+    public void Execute(Entity entity, int _, ref Sphere sphere, ref Translation spherePosition, [ReadOnly] ref Target target)
+    {
+        Entity targetEntity = target.Value;
+
+        Cylinder   cylinder         = Cylinders[targetEntity];
+        float3     cylinderPosition = Positions[targetEntity].Value;
+        quaternion cylinderRotation = Rotations[targetEntity].Value;
+
+        if (CollisionManager.CollideSphereCylinder(
+            cylinderRotation,
+            cylinderPosition,
+            cylinder.Length,
+            cylinder.Radius,
+            spherePosition.Value,
+            sphere.Radius))
+        {
+            var contact = new ContactPoint
+            {
+                EntityA = entity,
+                EntityB = targetEntity
+            };
+
+            Contacts.Enqueue(contact);
+        }
+    }
+}
+
+public class DetectCollisionSystem : JobComponentSystem
+{
+    public static NativeQueue<ContactPoint> Contacts;
+
+    protected override void OnCreate()
+    {
+        Contacts = new NativeQueue<ContactPoint>(Allocator.Persistent);
+    }
+
+    protected override void OnDestroy()
+    {
+        Contacts.Dispose();
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+        var cylinders    = GetComponentDataFromEntity<Cylinder>(true);
+        var translations = GetComponentDataFromEntity<Translation>(true);
+        var rotations    = GetComponentDataFromEntity<Rotation>(true);
+
+        var job = new DetectCollisionJob()
+        {
+            Cylinders = cylinders,
+            Positions = translations,
+            Rotations = rotations,
+            Contacts = Contacts.AsParallelWriter()
+        };
+    
+        return job.Schedule(this, inputDependencies);
+    }
+}
