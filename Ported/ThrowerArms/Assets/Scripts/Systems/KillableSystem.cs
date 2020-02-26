1a35d72c9c145c1088bef3010c37a0df7792a548
sbourque-eidos <58948191+sbourque-eidos@users.noreply.github.com>
2020-02-26 16:21:56 -0500

    Implement ScaleColliderSystem

--- Ported/ThrowerArms/Assets/Scripts/Systems/KillableSystem.cs
+++ Ported/ThrowerArms/Assets/Scripts/Systems/KillableSystem.cs
@@ -19,32 +19,32 @@ public class KillableSystem : SystemBase
         var planeData = GetComponentDataFromEntity<Plane>(isReadOnly: true);
 
         var sphereHandle = Entities.ForEach((Entity entity, int entityInQueryIndex, in KillableData killableData,
-            in LocalToWorld transform) =>
+            in LocalToWorld transform, in Sphere sphere) =>
         {
             Entity killPlaneEntity = killableData.TargetKillPlane;
             if (planeData.Exists(killPlaneEntity))
             {
                 Plane killPlane = planeData[killPlaneEntity];
-                if (CollisionManager.CollideSpherePlane(transform.Position, 1.0f, killPlane.Direction, killPlane.Distance))
+                if (CollisionManager.CollideSpherePlane(transform.Position, sphere.Radius, killPlane.Direction, killPlane.Distance))
                 {
                     sphereEcb.DestroyEntity(entityInQueryIndex, entity);
                 }
             }
-        }).WithAll<BallTag>().WithReadOnly(planeData).ScheduleParallel(Dependency);
+        }).WithReadOnly(planeData).ScheduleParallel(Dependency);
 
         var cylinderHandle = Entities.ForEach((Entity entity, int entityInQueryIndex, in KillableData killableData,
-            in LocalToWorld transform) =>
+            in LocalToWorld transform, in Cylinder cylinder) =>
         {
             Entity killPlaneEntity = killableData.TargetKillPlane;
             if (planeData.Exists(killPlaneEntity))
             {
                 Plane killPlane = planeData[killPlaneEntity];
-                if (CollisionManager.CollideCylinderPlane(transform.Rotation, transform.Position, 2.0f, 1.0f, killPlane.Direction, killPlane.Distance))
+                if (CollisionManager.CollideCylinderPlane(transform.Rotation, transform.Position, cylinder.Length, cylinder.Radius, killPlane.Direction, killPlane.Distance))
                 {
                     cylinderEcb.DestroyEntity(entityInQueryIndex, entity);
                 }
             }
-        }).WithAll<CanTag>().WithReadOnly(planeData).ScheduleParallel(sphereHandle);
+        }).WithReadOnly(planeData).ScheduleParallel(sphereHandle);
 
         Dependency = cylinderHandle;
         m_EcbSystem.AddJobHandleForProducer(Dependency);
