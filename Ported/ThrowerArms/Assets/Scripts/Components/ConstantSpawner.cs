957593850517b972e9e8ff88ba27dea985ce6e9c
hlemerise <>
2020-02-26 11:36:27 -0500

    First draft spawner

---
+++ Ported/ThrowerArms/Assets/Scripts/Components/ConstantSpawner.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Mathematics;
+
+public struct ConstantSpawner : IComponentData
+{
+    public float3 MinArea;
+    public float3 MaxArea;
+    public Entity Prefab;
+}
---
+++ Ported/ThrowerArms/Assets/Scripts/Components/ConstantSpawnerAuthoring.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class ConstantSpawnerAuthoring : MonoBehaviour, IConvertGameObjectToEntity, IDeclareReferencedPrefabs
+{
+    public GameObject Prefab;
+    public GameObject SpawnArea;
+    public GameObject DeathPlane;
+
+    
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        var bounds = SpawnArea.GetComponent<Renderer>().bounds;
+
+        dstManager.AddComponentData(entity, new ConstantSpawner
+        {
+            MinArea = bounds.min,
+            MaxArea = bounds.max,
+            Prefab = conversionSystem.GetPrimaryEntity(Prefab)
+        });
+    }
+    
+    public void DeclareReferencedPrefabs(List<GameObject> referencedPrefabs)
+    {
+        referencedPrefabs.Add(Prefab);
+    }
+}
