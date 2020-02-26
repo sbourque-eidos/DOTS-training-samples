c2e5741b5f86c8f67f3509454d57782b2738324d
jmtannous <jmtannous@eidosmontreal.com>
2020-02-26 14:00:30 -0500

    added increase scale system
    reviews: alex, hugo

---
+++ Ported/ThrowerArms/Assets/Scripts/Components/IncreaseScaleData.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+[GenerateAuthoringComponent]
+public struct IncreaseScaleData : IComponentData
+{
+    public float Speed;
+    public float PercentReached; //[0,1]
+    public float TargetScale; //[0,1]
+}
---
+++ Ported/ThrowerArms/Assets/Scripts/Systems/IncreaseScaleSystem.cs
@@ -0,0 +1,50 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using static Unity.Mathematics.math;
+
+public class IncreaseScaleSystem : SystemBase
+{
+    private EndSimulationEntityCommandBufferSystem m_ecb;
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+        m_ecb = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
+    protected override void OnUpdate()
+    {
+        var ecb = m_ecb.CreateCommandBuffer().ToConcurrent();
+
+        var dt = UnityEngine.Time.deltaTime;
+
+        Entities.ForEach((
+                Entity e,
+                int entityInQueryIndex,
+                ref IncreaseScaleData increaseScaleData,
+                ref NonUniformScale nonUniformScale) =>
+            {
+                //compute new scale
+                float newPercentageReached = increaseScaleData.PercentReached + (increaseScaleData.Speed * dt);
+                newPercentageReached = math.clamp(newPercentageReached, 0, 1);
+
+                float newScale = newPercentageReached * increaseScaleData.TargetScale;
+
+                //update scale values
+                nonUniformScale.Value = newScale;
+                increaseScaleData.PercentReached = newPercentageReached;
+
+                if(newScale == increaseScaleData.TargetScale)
+                {
+                    //remove component
+                    ecb.RemoveComponent<IncreaseScaleData>(entityInQueryIndex, e);
+                }
+            })
+            .ScheduleParallel();
+
+        m_ecb.AddJobHandleForProducer(Dependency);
+    }
+}
