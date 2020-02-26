1d8d8b165947a9e6c764c289355a8c5b48803e16
sbourque-eidos <58948191+sbourque-eidos@users.noreply.github.com>
2020-02-26 16:21:56 -0500

    Implement ScaleColliderSystem

---
+++ Ported/ThrowerArms/Assets/Scripts/Systems/ScaleColliderSystem.cs
@@ -0,0 +1,23 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+
+[UpdateBefore(typeof(DetectCollisionSystem))]
+[UpdateBefore(typeof(KillableSystem))]
+public class ScaleColliderSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        var sphereHandle = Entities.ForEach((ref Sphere sphere, in NonUniformScale scale) =>
+        {
+            sphere.Radius = scale.Value.x * 0.5f;
+        }).ScheduleParallel(Dependency);
+
+        var cylinderHandle = Entities.ForEach((ref Cylinder cylinder, in NonUniformScale scale) =>
+        {
+            cylinder.Length = scale.Value.y;
+            cylinder.Radius = scale.Value.x * 0.5f;
+        }).ScheduleParallel(sphereHandle);
+
+        Dependency = cylinderHandle;
+    }
+}
