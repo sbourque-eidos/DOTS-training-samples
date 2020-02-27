dbd9df9fc818ddf21b740b3b20cd5c3101c65f6d
sbourque-eidos <58948191+sbourque-eidos@users.noreply.github.com>
2020-02-27 09:51:42 -0500

    Add arm states

---
+++ Ported/ThrowerArms/Assets/Scripts/Systems/ArmStateSystem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class ArmStateSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref ArmStateData state) =>
+        {
+            switch (state.CurrentState)
+            {
+                case ArmStateData.State.TargetingBall:
+                    break;
+                case ArmStateData.State.PickingUp:
+                    break;
+                case ArmStateData.State.WindingUp:
+                    break;
+                case ArmStateData.State.TargetingCan:
+                    break;
+                case ArmStateData.State.Throwing:
+                    break;
+            }
+        }).ScheduleParallel();
+    }
+}
