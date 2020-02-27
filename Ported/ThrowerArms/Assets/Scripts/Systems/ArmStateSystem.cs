88847d8e3a33822f8b93526ec134a19bd4ef20a7
hlemerise <>
2020-02-27 13:59:36 -0500

    Add hardcoded ground

--- Ported/ThrowerArms/Assets/Scripts/Systems/ArmStateSystem.cs
+++ Ported/ThrowerArms/Assets/Scripts/Systems/ArmStateSystem.cs
@@ -4,11 +4,30 @@ using Unity.Mathematics;
 
 public class ArmStateSystem : SystemBase
 {
+    struct Ground : IComponentData
+    {
+    }
+
     EntityCommandBufferSystem m_EcbSystem;
 
     protected override void OnCreate()
     {
         m_EcbSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+
+        var ground = EntityManager.CreateEntity(typeof(Ground));
+        EntityManager.AddComponentData(ground, new Plane()
+        {
+            Direction = new float3(0.0f, 1.0f, 0.0f),
+            Distance = -5.0f
+        });
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+
+        var groundEntity = GetSingletonEntity<Ground>();
+        EntityManager.DestroyEntity(groundEntity);
     }
 
     protected override void OnUpdate()
@@ -20,6 +39,7 @@ public class ArmStateSystem : SystemBase
         var targetData = GetComponentDataFromEntity<Target>(isReadOnly: true);
         var translationData = GetComponentDataFromEntity<Translation>();
 
+        var groundEntity = GetSingletonEntity<Ground>();
         var dt = Time.DeltaTime;
 
         var handle = Entities.ForEach((Entity entity, int entityInQueryIndex, ref ArmStateData state, in LocalToWorld transform) =>
@@ -60,6 +80,7 @@ public class ArmStateSystem : SystemBase
                         state.CurrentState = ArmStateData.State.RequestingBallTargeting;
                         ecb.AddComponent(entityInQueryIndex, target.Value, new Velocity { Value = new float3(0.0f, 4.0f, 4.0f) });
                         ecb.AddComponent<FreeFalling>(entityInQueryIndex, target.Value);
+                        ecb.SetComponent(entityInQueryIndex, target.Value, new KillableData() { TargetKillPlane = groundEntity });
                         ecb.AddComponent<TargetingCanTag>(entityInQueryIndex, entity);
                         ecb.RemoveComponent<Target>(entityInQueryIndex, entity);
                     }
