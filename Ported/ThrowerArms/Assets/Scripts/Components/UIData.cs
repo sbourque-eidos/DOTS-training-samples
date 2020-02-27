878f7622189704a796e27f9b5800bbdf5ea386a5
jmtannous <jmtannous@eidosmontreal.com>
2020-02-27 16:48:28 -0500

    add UI, first pass

---
+++ Ported/ThrowerArms/Assets/Scripts/Components/UIData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[Serializable]
+public struct UIData : IComponentData
+{
+    public NativeString64 canCount;
+    public NativeString64 ballCount;
+
+}
---
+++ Ported/ThrowerArms/Assets/Scripts/Systems/UISystem.cs
@@ -0,0 +1,39 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+
+public class UISystem : SystemBase
+{
+    private EntityQuery m_cylindersQuery;
+    private EntityQuery m_ballQuery;
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+
+        var uiDataEntity = EntityManager.CreateEntity(typeof(UIData));
+
+        m_cylindersQuery = GetEntityQuery(typeof(CanTag));
+        m_ballQuery = GetEntityQuery(typeof(BallTag));
+
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+
+        var uiDataEntity = GetSingletonEntity<UIData>();
+        EntityManager.DestroyEntity(uiDataEntity);
+    }
+
+    protected override void OnUpdate()
+    {
+        var uiData = GetSingleton<UIData>();
+        var canCount = m_cylindersQuery.CalculateEntityCount();
+        var ballCount = m_ballQuery.CalculateEntityCount();
+
+        uiData.canCount.CopyFrom(canCount.ToString());
+        uiData.ballCount.CopyFrom(ballCount.ToString());
+
+        var uiDataEntity = GetSingletonEntity<UIData>();
+        EntityManager.SetComponentData<UIData>(uiDataEntity, uiData);
+    }
+}
