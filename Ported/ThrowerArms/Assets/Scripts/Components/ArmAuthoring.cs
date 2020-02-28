f5b8b6989677db95f86ed91233c455257be0ad5f
hlemerise <>
2020-02-28 10:31:39 -0500

    Arm authoring iterationf

--- Ported/ThrowerArms/Assets/Scripts/Components/ArmAuthoring.cs
+++ Ported/ThrowerArms/Assets/Scripts/Components/ArmAuthoring.cs
@@ -1,17 +1,18 @@
 ﻿
-using UnityEngine;
+using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Rendering;
 using Unity.Transforms;
-using Unity.Collections;
+using UnityEngine;
 
-public class ArmAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+public class ArmAuthoring : MonoBehaviour, IConvertGameObjectToEntity, IDeclareReferencedPrefabs
 {
 	public float armBoneLength;
 	public float armBoneThickness;
 	public float armBendStrength;
 	public float maxReachLength;
-	public float reachDuration;
 	public float maxHandSpeed;
 	[Space(10)]
 	public float[] fingerBoneLengths;
@@ -25,48 +26,45 @@ public class ArmAuthoring : MonoBehaviour, IConvertGameObjectToEntity
 	public float thumbBendStrength;
 	public float thumbXOffset;
 	[Space(10)]
-	public float windupDuration;
-	public float throwDuration;
-	public AnimationCurve throwCurve;
-	public float baseThrowSpeed;
-	public float targetXRange;
-	[Space(10)]
-	public Material material;
-	public Mesh boneMesh;
-   
-	static readonly float3 k_Right = new float3(1.0f, 0.0f, 0.0f);
+	public GameObject bonePrefab;
 
-	void IConvertGameObjectToEntity.Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    public void DeclareReferencedPrefabs(List<GameObject> referencedPrefabs)
+    {
+		referencedPrefabs.Add(bonePrefab);
+    }
+
+    void IConvertGameObjectToEntity.Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
 	{
-		ComponentType[] componentTypes = new ComponentType[]
+		ComponentType[] armComponentTypes = new ComponentType[]
         {
 		    typeof(JointEntity),
-		    typeof(SkeletonReference),
-		    typeof(Translation)
+		    typeof(SkeletonReference)
         };
-
-		ComponentType[] jointTypes = new ComponentType[]
-		{
-	        typeof(Translation),
-	        typeof(Rotation),
-	        typeof(Parent)
-		};
 
 		const int segmentCount = 3;
 
 		BlobAssetReference<Skeleton> skeletonRef = SkeletonAssetBuilder.BuildSkeleton();
 		var skeletonRefComponent = new SkeletonReference { skeleton = skeletonRef };
 
-		var comps = new ComponentTypes(componentTypes);
+		var comps = new ComponentTypes(armComponentTypes);
 		dstManager.AddComponents(entity, comps);
 
 		dstManager.AddComponentData(entity, skeletonRefComponent);
 
+		var parent = entity;
+
 		var joints = new NativeArray<Entity>(segmentCount + 1, Allocator.Temp);
 		for (int jointIndex = 0; jointIndex < segmentCount+1; jointIndex++)
 		{
-			Entity newJoint = dstManager.CreateEntity(jointTypes);
+			var newJoint = dstManager.Instantiate(conversionSystem.GetPrimaryEntity(bonePrefab));
+
 			joints[jointIndex] = newJoint;
+
+			dstManager.AddComponentData(newJoint, new Parent { Value = parent });
+			dstManager.AddComponentData(newJoint, new LocalToParent { Value = float4x4.identity });
+			dstManager.AddComponentData(newJoint, new NonUniformScale { Value = new float3(1.0f) });
+
+			parent = newJoint;
 		}
 
 		var buffer = dstManager.AddBuffer<JointEntity>(entity);
@@ -74,5 +72,7 @@ public class ArmAuthoring : MonoBehaviour, IConvertGameObjectToEntity
 		{
 			buffer.Add(new JointEntity { Value = joints[jointIndex] });
 		}
+
+		joints.Dispose();
 	}
 }
